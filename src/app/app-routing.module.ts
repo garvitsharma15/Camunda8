9d8d3e4da7ae17211d26aa6501c109c59f89935f
mendusakethreddy@gmail.com <mendusakethreddy@gmail.com>
2022-12-26 14:25:17 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompleteCaseComponent } from './complete-case/complete-case.component';
+import { CreateCaseComponent } from './create-case/create-case.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CreateCaseComponent },
+  { path: 'createcase', component: CreateCaseComponent },
+  { path: 'completecase', component: CompleteCaseComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompleteCaseComponent } from './complete-case/complete-case.component';
+import { CreateCaseComponent } from './create-case/create-case.component';
+import { GraphQLModule } from './graphql.module';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CreateCaseComponent,
+    CompleteCaseComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    GraphQLModule
   ],
   providers: [],
   bootstrap: [AppComponent]
