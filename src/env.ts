953a755cf318f518680d6f649d531ed74b1182c9
Garvit Sharma <garvitsharma15@gmail.com>
2023-05-07 14:58:39 +0530

    changing creds

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,6 @@
-export const url = "https://jfk-1.tasklist.camunda.io/9978e1b9-ec02-4407-becc-65030e9b1e70"
-export const backend_url = "10.1.59.108"
+// export const url = "https://jfk-1.tasklist.camunda.io/20a43018-b4f5-49a8-a58e-ea3581201a16"
+// export const backend_url = "localhost"
 
 
-export const client_id = "c87L5NL-QKq9ULrqcPjzQaIu9fVe.Or."
-export const client_secret = "sbenvDz4v8CK7oJCQuejIFt9TpyASycID0REkmw4VX86fX2e0SPTTP74gOf-ZWxn"
+// export const client_id = "MXLgybhCvSLQMRJ3dNa1EDyRaSzIcAuW"
+// export const client_secret = "0gVWZ2tXVk-adG-MEgMEUlem3wKAOc9z9H3GjXYDVx7rvmE-FnbdfNA2AWW3xW94"
